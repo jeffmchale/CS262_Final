536c003b079d6ef3a6edd21e51b8b13244a6a7dd
Jeff McHale <46632270+jeffmchale@users.noreply.github.com>
2020-03-21 20:51:54 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ class Main{
         Database db = new Database("192.154.227.17", "CGCC", 1444, "jeff_db", false, "jeff", "jeff7409!");
         //Instantiate Dam Classes - these queries will show all of the information
         Customer c = new DamCustomer(db).selectCustomerByID("1");
-        LineItem ln = new DamLineItem(db).selectItemById("Fender Stratocaster", "1");
+        LineItem ln = new DamLineItem(db).selectItemById("Gibson Les Paul", "1");
         Order o = new DamOrder(db).selectOrderByID("1");
         Product p = new DamProduct(db).selectProductByName("value");
         Payment pm = new DamPayment(db).selectPaymentByID("55555");
